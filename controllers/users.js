f89e1c243545194aca341b66646d149035962a2f
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-04-05 08:51:42 +0300

    корректировка кода для контроллеров пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -45,7 +45,7 @@ module.exports.updateUserProfile = (req, res) => {
   const { name, about } = req.body;
 
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
-    .orFail(() => { throw new Error('DocumentNotFoundError'); })
+    .orFail()
     .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
@@ -61,7 +61,7 @@ module.exports.updateUserProfile = (req, res) => {
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .orFail(() => { throw new Error('DocumentNotFoundError'); })
+    .orFail()
     .then((user) => res.send({ user }))
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
