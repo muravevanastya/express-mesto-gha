c19cd424cbc27bbe06ad6d3108ffad1ff8ea016c
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-04-09 09:11:46 +0300

    создала мидлвэр для авторизации

---
+++ middlewares/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+  const token = req.cookies.jwt;
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'super-strong-secret');
+  } catch {
+    res.status(401).send({ message: 'Необходима авторизация' });
+  }
+
+  req.user = payload;
+  next();
+};
