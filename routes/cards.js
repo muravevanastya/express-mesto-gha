cf4e2c36b80e73ea8971567c3c2bc7f9bf927051
Anastasia Muravyeva <muravevanastya@gmail.com>
2023-04-12 19:28:40 +0300

    создала валидацию запросов карточек

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,34 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const { getAllCards } = require('../controllers/cards');
 const { createCard } = require('../controllers/cards');
 const { deleteCard } = require('../controllers/cards');
 const { likeCard } = require('../controllers/cards');
 const { dislikeCard } = require('../controllers/cards');
 
+const urlRegex = /https?:\/\/(www.)?[0-9a-z\-.]{1,}\.\w{1,}((\/[a-z0-9-._~:?#[\]@!$&'()*+,;=]{1,}){1,}\/?#?)?/;
+
 router.get('/', getAllCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().pattern(urlRegex),
+  }),
+}), createCard);
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24).required(),
+  }),
+}), deleteCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24).required(),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24).required(),
+  }),
+}), dislikeCard);
 
 module.exports = router;
